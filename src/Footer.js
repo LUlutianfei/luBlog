9e1a1239fc7714c317c3369fde2d019a586b13f8
lutianfei <1172486783@qq.com>
2019-08-21 22:48:42 +0800

    footerQR update

--- src/Footer.js
+++ src/Footer.js
@@ -3,12 +3,20 @@ import './css/Footer.css'
 import QQ from './img/qq.svg';
 import WeChat from './img/wechat.svg';
 import WeiBo from './img/weibo.svg';
-import Er from './img/er.svg';
+import QQImg from './img/QQ.jpg';
+import WeChatImg from './img/WeChat.png';
+import WeiBoImg from './img/WeiBo.png';
+
 
 class Footer extends React.Component{
 	constructor(props){
 		super(props);
 		this.showQQ = this.showQQ.bind(this);
+		this.showWeChat = this.showWeChat.bind(this);
+		this.showWeiBo = this.showWeiBo.bind(this);
+		this.hideQQ = this.hideQQ.bind(this);
+		this.hideWeChat = this.hideWeChat.bind(this);
+		this.hideWeiBo = this.hideWeiBo.bind(this);
 		this.state = {
 			qq:false,
 			wechat:false,
@@ -20,14 +28,43 @@ class Footer extends React.Component{
 			qq:true
 		})
 	}
+	showWeChat(){
+		this.setState({
+			wechat:true
+		})
+	}
+	showWeiBo(){
+		this.setState({
+			weibo:true
+		})
+	}
+	hideQQ(){
+		this.setState({
+			qq:false
+		})
+	}
+	hideWeChat(){
+		this.setState({
+			wechat:false
+		})
+	}
+	hideWeiBo(){
+		this.setState({
+			weibo:false
+		})
+	}
 	render(){
 		return(
 			<footer>
+				<div className="code" >
+					{this.state.qq?(<img className="qr QQ_QR" src={QQImg} alt="qq二维码"/>):null}
+					{this.state.wechat?(<img className="qr WeChat_QR" src={WeChatImg} alt="微信二维码"/>):null}
+					{this.state.weibo?(<img className="qr WeiBo_QR" src={WeiBoImg} alt="微博二维码"/>):null}
+				</div>
 				<div className="connect" >
-					<img onMouseOver={this.showQQ}  alt="qq" src={QQ} />
-					{this.state.qq?(<img src={Er} alt="qq二维码"/>):null}
-					<img onMouseOver={this.showWeChat}  alt="wechat" src={WeChat} />
-					<img onMouseOver={this.showWeibo}  alt="weibo" src={WeiBo}/>
+					<img className="icon"  onMouseOver={this.showQQ} onMouseOut={this.hideQQ}  alt="qq" src={QQ} />
+					<img className="icon"  onMouseOver={this.showWeChat} onMouseOut={this.hideWeChat}  alt="wechat" src={WeChat} />
+					<img className="icon"  onMouseOver={this.showWeiBo} onMouseOut={this.hideWeiBo}  alt="weibo" src={WeiBo}/>
 				</div>
 			</footer>
 		)
